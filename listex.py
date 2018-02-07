18d103cf0bbc8c887402c3922d7b19bb284b5513
Cristobal <manzanares.cristobal@gmail.com>
2018-02-07 11:56:52 -0600

    loading files inside exercises

--- listex.py
+++ listex.py
@@ -1,2 +1,21 @@
 import numpy
+import math
+#Problem 1
 list = numpy.random.random((1,5))
+sumnum = sum(list)
+
+#Problem 2
+x = 0
+larg = list[0]
+if (x+1) < len(list):
+    if list[x+1] > larg:
+        larg = list[x+1]
+        x += 1
+    else:
+        x += 1
+else:
+    print(larg)
+
+
+
+largnum = max(list)
