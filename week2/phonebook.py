194206ff99fe744ecefbf2fbc8aa162dc3d4bf37
Cristobal <manzanares.cristobal@gmail.com>
2018-02-12 14:33:19 -0600

    WeeK 2 DAY 1 finalized

---
+++ week2/phonebook.py
@@ -0,0 +1,26 @@
+
+
+while True:
+    print('Electronic Phone Book')
+    print('=====================')
+    print('1. Look up an entry')
+    print('2. Set an entry')
+    print('3. Delete an entry')
+    print('4. List all entries')
+    print('5. Quit')
+    choice = int(input('What do you want to do? (1-5) '))
+    if choice == 1:
+    
+
+
+    elif choice == 2:
+    
+
+
+    elif choice == 3:
+
+    elif choice == 4:
+    
+    elif choice == 5:
+        print('Byë')
+        break
