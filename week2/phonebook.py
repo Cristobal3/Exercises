d91c250b25c878aa91d2171e7a9f1471d1ff86d4
Cristobal <manzanares.cristobal@gmail.com>
2018-02-12 14:34:44 -0600

    Small change

--- week2/phonebook.py
+++ week2/phonebook.py
@@ -1,5 +1,5 @@
 
-
+book = {'Melissa': '584-394-5857', 'Igor': '857-485-2935'}
 while True:
     print('Electronic Phone Book')
     print('=====================')
@@ -10,16 +10,30 @@ while True:
     print('5. Quit')
     choice = int(input('What do you want to do? (1-5) '))
     if choice == 1:
-    
-
+        person = input('Name: ')
+        if book.get(person, 'new') == 'new':
+            print('That person is not in your contacts. Please try again.')
+        else:
+            print('Found entry for Melissa: ', book[person])
 
     elif choice == 2:
-    
-
+        person = input('Name: ')
+        number = input('Phone Number: ')
+        book[person] = number
+        print('Entry stored for', person,'.')
 
     elif choice == 3:
+        person = input('Name: ')
+        if book.get(person, 'new') == 'new':
+            print('That person is not in your contacts. Please try again.')
+        else:
+            del book[person]
+            print('Deleted entry for', person)
 
     elif choice == 4:
+        for x in book:
+            print('Found entry for {person}: {number}'.format(person=x, number=book[x]))
+
     
     elif choice == 5:
         print('Byë')
