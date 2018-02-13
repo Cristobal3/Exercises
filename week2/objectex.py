eafff9e1bcd2dad361c071250283414258bed6cc
Cristobal <manzanares.cristobal@gmail.com>
2018-02-13 12:09:16 -0600

    Object ex 1 complete

---
+++ week2/objectex.py
@@ -0,0 +1,48 @@
+
+#Problem 1
+class Person:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        self.friends = []
+        self.greeting_count = 0
+    
+    def __str__(self):
+        return 'Person: {} {} {}'.format(self.name, self.email, self.phone)
+
+    def greet(self, other_person):
+        print('Hello {}, I am {}!'.format(other_person.name, self.name))
+        self.greeting_count += 1
+
+    def print_contact_info(self):
+        print('{}\'s email is {}, {}\'s phone number: {}'.format(self.name, self.email, self.name, self.phone))
+    
+    def add_friend(self,friend):
+        jordan.friends.append(friend.name)
+    
+    def num_friends(self):
+        print(len(jordan.friends))
+    
+
+
+
+sonny = Person('Sonny', 'sonny@hotmail.com', '483-485-4948')
+jordan = Person('Jordan', 'jordan@aol.com', '495-586-3456')
+
+sonny.greet(jordan)
+jordan.greet(sonny)
+print('Contact info of {} is {} and {}'.format(sonny.name, sonny.email, sonny.phone))
+print('Contact info of {} is {} and {}'.format(jordan.name, jordan.email, jordan.phone))
+
+#Prblem 2
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+    
+    def print_info(self):
+        print('{} {} {}'.format(name.year,name.make,name.model))
+
+       
