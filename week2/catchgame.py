5e171b3b8f7f08558f5a93b8eb55015178ee5f14
Cristobal <manzanares.cristobal@gmail.com>
2018-02-16 12:39:34 -0600

    Catch game: moving hero, moving monster update

--- week2/catchgame.py
+++ week2/catchgame.py
@@ -15,28 +15,31 @@ class Monster:
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
         self.dir = ['n','s','w','e']
+        self.ch = 'e'
     def load_image(self, it):
-        ch = 'e'
-        '''
-        if it % 120 == 0:
-            ch = self.dir[random.randint(0,3)]
-        if ch == 'n':
+        if it%120 == 0:
+            self.ch = self.dir[random.randint(0,3)]
+        
+
+        if self.ch == 'n':
             self.rect[1] += 1
-        elif ch == 's':
+        elif self.ch == 's':
             self.rect[1] -= 1
-        elif ch == 'w':
+        elif self.ch == 'w':
             self.rect[0] -= 1
         else:
             self.rect[0] += 1    
-        '''
+        
         if self.rect[0] > 500:
             self.rect[0] = 0
         elif self.rect[0] < 0:
             self.rect[0] = 500
-        elif self.rect[1] > 500:
-            self.rect[1] = -500
+        elif self.rect[1] > 460:
+            self.rect[1] = 0
+        elif self.rect[1] < 0 :
+            self.rect[1] = 420
         else:
-            self.rect[1] = 500
+            pass
         
         
 
@@ -45,28 +48,36 @@ class Hero:
         self.image = pygame.image.load(image_file)
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
-    def load_image(self):
-        screen.blit(self.image, self.rect)
-        self.rect[0] += 5
-        if self.rect[0] > 500:
-            self.rect[0] = 0
+    def move(self, keys_pressed):
+        if keys_pressed[K_LEFT]:
+            self.rect -= 5
+
+        if keys_pressed[K_RIGHT]:
+            self.rect += 5
+
+        if keys_pressed[K_UP]:
+            self.rect -= 5
+
+        if keys_pressed[K_DOWN]:
+            self.rect += 5
 
 
 
 def main():
-    width = 500
-    height = 500
+    width = 510
+    height = 480
     blue_color = (97, 159, 182)
-    it = 0
+    
     
     pygame.init()
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('My Game')
     background = Background('background.png',[0,0])
     monster = Monster('./images/monster.png', [5,5])
-    hero = Monster('./images/hero.png', [250,250])
+    hero = Hero('./images/hero.png', [250,250])
     change_dir_countdown = 120
     clock = pygame.time.Clock()
+    it = 0
 
     # Game initialization
     
@@ -82,7 +93,22 @@ def main():
 
 
         # Game logic
+        monster.load_image(it)
+        keys_pressed = pygame.key.get_pressed()
+        
+        
+        if keys_pressed[pygame.K_LEFT]:
+            hero.rect[0] -= 5
 
+        if keys_pressed[pygame.K_RIGHT]:
+            hero.rect[0] += 5
+
+        if keys_pressed[pygame.K_UP]:
+            hero.rect[1] -= 5
+
+        if keys_pressed[pygame.K_DOWN]:
+            hero.rect[1] += 5
+        
         # Draw background
         #screen.fill(blue_color)
         screen.fill([255, 255, 255])
@@ -92,7 +118,7 @@ def main():
         screen.blit(hero.image, hero.rect)
         
         #Game logic
-        monster.load_image(it)
+        
         
         # Game display
 
